0e85c0cccf93edb770a57b99f42887f6b99223ab
xushishu <457683048@qq.com>
2020-08-14 18:44:16 +0800

    aaa

---
+++ src/main/java/com/xss/utilsproject/UtilsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.xss.utilsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilsProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/xss/utilsproject/util/DateUtils.java
@@ -0,0 +1,9 @@
+package com.xss.utilsproject.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DateUtils {
+
+
+}

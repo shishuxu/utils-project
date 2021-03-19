fc9206962e2e301d38491a030b692102184c6a31
xushishu <457683048@qq.com>
2021-03-19 11:43:28 +0800

    //

--- src/main/java/com/xss/utilsproject/util/DateUtils.java
+++ src/main/java/com/xss/utilsproject/util/DateUtils.java
@@ -168,7 +168,7 @@ public class DateUtils {
             cal.setTime(smdate);
             long time1 = cal.getTimeInMillis();
             cal.setTime(bdate);
-            long time2 = cal.getTimeInMillis();
+            long time2 = cal.getTimeInMillis();//
             long between_days = (time2 - time1) / (1000 * 3600 * 24);
 
             return Integer.parseInt(String.valueOf(between_days));
